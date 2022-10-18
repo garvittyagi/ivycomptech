e0215a86982aea4de388ff2c0125e9b35145ad9d
garvittyagi <garvittyagi99@gmail.com>
2022-10-18 13:35:33 +0530

    14/10/22

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
-
+import {FormControl, FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ReactiveForm';
+  registrationForm = new FormGroup({
+    UserName: new FormControl(''),
+    Password: new FormControl(''),
+    ConfirmPassword: new FormControl(''),
+    address: new FormGroup({
+      city: new FormControl(''),
+      state: new FormControl(''),
+      PinCode: new FormControl(''),
+    })
+  });
+  getValues(){
+    alert(this.registrationForm.controls['address'].value.PinCode);
+   console.log(this.registrationForm.value);
+   console.log(this.registrationForm.controls['UserName'].value);
+   console.log(this.registrationForm.controls['address'].value);
+  }
 }
